f8c9d28b06b5f44d7d0032135eefec184ec2c842
Lar <Lar@Lars-MacBook-Pro.local>
2016-12-09 20:19:35 +0000

    Hosting issue: access json file fix

--- mainController.js
+++ mainController.js
@@ -23,7 +23,7 @@ var myapp = angular.module('education',[]);
 myapp.controller('educationCtrl',function($scope,$http){
     $scope.result = null;
     
-    $http({method: 'GET', url: 'Results.json'})
+    $http({method: 'GET', url: './Results.json'})
     .success(function(data, status, headers, config) {
         $scope.results=data;
     })
